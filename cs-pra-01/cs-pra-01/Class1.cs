aaa33bbbb284a62e001a1974177985ef3773d15c
SatakeYusuke19920527 <satkyusk@icloud.com>
2021-05-17 11:35:29 +0900

    cs pra

---
+++ cs-pra-01/cs-pra-01/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cs_pra_01
+{
+    class Class1
+    {
+        public static void classFunc()
+        {
+            Console.WriteLine("Hello Class");
+        }
+    }
+}
---
+++ cs-pra-01/cs-pra-01/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace cs_pra_01
+{
+    class Program
+    {
+        public string name;
+        public int age;
+
+        static void Main(string[] args)
+        {
+            Program test = new Program();
+            test.setAge(1999);
+            test.setName("aaaa");
+            firstFunc();
+            Console.WriteLine(test.getName() + "Hello World!" + test.getAge());
+            Class1.classFunc();
+        }
+
+        public static int firstFunc()
+        {   
+            Console.WriteLine("firstFunc");
+            return 0;
+        }
+
+        public void setAge(int age)
+        {
+            this.age = age;
+        }
+
+        public int getAge()
+        {
+            return this.age;
+        }
+
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+        public string getName()
+        {
+            return this.name;
+        }
+    }
+}
